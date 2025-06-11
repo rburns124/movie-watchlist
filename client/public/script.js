eef0bb5766bba682d0f6ec8f1101c71bef9cda6b
Robert Burns <rrburns18@gmail.com>
2025-06-10 21:54:14 -0600

    Revert "Implement new header layout and scripts"

--- client/public/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const menuToggle = document.querySelector('.menu-toggle');
-  const navList = document.querySelector('nav ul');
-  if (menuToggle) {
-    menuToggle.addEventListener('click', () => {
-      navList.classList.toggle('show');
-    });
-  }
-});
